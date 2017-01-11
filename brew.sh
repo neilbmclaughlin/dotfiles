4ce4ec24aa428f29ef8862646b9851cf19bf28f7
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-01-11 13:04:06 +0000

    add docker

--- brew.sh
+++ brew.sh
@@ -14,7 +14,7 @@ fi
 sudo chown -R $(whoami):admin /usr/local
 
 # Utils
-brews=(git bash-completion hub heroku the_silver_searcher cmake pass gnupg jq azure-cli)
+brews=(git bash-completion hub heroku the_silver_searcher cmake pass gnupg jq azure-cli docker)
 
 # THE editor?!
 brews+=(vim)
