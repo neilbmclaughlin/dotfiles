8c5ce56ffb710ee284a062f8bbfdb4dfd91673eb
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-10-05 18:25:52 +0100

    more nvm tweaks so that vim bundle install (which uses node) will run

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,1 +1,7 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+
+#above script modifies env settings in .bashrc so re-run it
+
+. .bashrc
+
+nvm alias default ${NODE_VERSION:8.4.0}
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-./install-dotfiles.sh
-./install-brew.sh
-./install-gem.sh
-./install-pip.sh
-./install-nvm.sh
-./config-vim.sh
+. ./install-dotfiles.sh
+. ./install-brew.sh
+. ./install-gem.sh
+. ./install-pip.sh
+. ./install-nvm.sh
+. ./config-vim.sh
 
