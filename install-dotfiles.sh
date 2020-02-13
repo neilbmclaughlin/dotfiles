e1585b964494068dbc7a9684d55fb5c50bacda0f
neil mclaughlin <neil.mclaughlin4@nhs.net>
2020-02-13 00:27:02 +0000

    Remove unused config

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,17 +4,15 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 export DOTFILES_DIR
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTFILES_DIR="$( pwd )"
 
 printf "${GREEN}Starting to symlink...${NC}\n"
 
 ln -nfsv "$DOTFILES_DIR/.zshrc" ~
+ln -nfsv "$DOTFILES_DIR/.tmux.conf" ~
 ln -nfsv "$DOTFILES_DIR/.gitconfig" ~
 ln -nfsv "$DOTFILES_DIR/.aliases" ~
 ln -nfsv "$DOTFILES_DIR/.colors" ~
-ln -nfsv "$DOTFILES_DIR/.prompt" ~
-ln -nfsv "$DOTFILES_DIR/.git-prompt.sh" ~
-ln -nfsv "$DOTFILES_DIR/.git-completion.bash" ~
 ln -nfsv "$DOTFILES_DIR/.vimrc" ~
 ln -nfsv "$DOTFILES_DIR/.tern-project" ~
 ln -nfsv "$DOTFILES_DIR/.vim/doc" ~/.vim
