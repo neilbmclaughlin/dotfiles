34662f6c2c4864410526b016c870cd1b6717276d
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2020-02-18 15:47:43 +0000

    Populate KUBECONFIG with EKS dev cluster 001 config path

--- install-kubctl.sh
+++ install-kubctl.sh
@@ -7,3 +7,7 @@ else
   rm -Rf ~/.kube/configs/ffcldneksawss001
 fi
 git clone http://gitlab.ffc.aws-int.defra.cloud/kubernetes/ffcldneksawss001.git ~/.kube/configs/ffcldneksawss001 
+X="export KUBECONFIG=~/.kube/configs/local/config:~/.kube/configs/ffcldneksawss001/config"
+[ $(grep --count "$X"  ~/.zshrc) -eq 0 ] && echo $X >> ~/.zshrc
+
+. ~/.zshrc
